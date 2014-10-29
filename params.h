33cb2fd9e822ea50ef3571ac5ec022426805a02d
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-10-29 12:13:25 +0100

    Inclreased maximum ensemble size to 230, hope that is enough and does not break programs

--- params.h
+++ params.h
@@ -3,5 +3,5 @@
      &        ,nensmax,nmodelmax
         parameter (nxmax=1650,nymax=697,nzmax=95,                         &
      &       yrbeg=1,yrend=2300,nvmax=1,npermax=73,                       &
-     &       ndata=npermax*(yrend-yrbeg+1),nensmax=120,                   &
+     &       ndata=npermax*(yrend-yrbeg+1),nensmax=230,                   &
      &       nmodelmax=30)
