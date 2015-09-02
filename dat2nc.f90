e5d38aaf14d9a2cccd808bef9aebda5d8a2e29dd
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-09-02 15:01:43 +0200

    Fixed a big bug: dmake sure not to read data from an old netcdf file while generating a new one...

--- dat2nc.f90
+++ dat2nc.f90
@@ -8,9 +8,9 @@ program dat2nc
     integer             :: yr,mo,nperyear,i,j,n,mens,mens1
     real*4              :: x,y,x0,y0
     real*4,allocatable  :: data(:,:,:)
-    character           :: file*256,since*32,var*40,lvar*80,description*128
+    character           :: file*256,ncfile*256,since*32,var*40,lvar*80,description*128
     character           :: type*1,comment*10000,line*80,units*20
-    logical             :: lwrite,lstandardunits
+    logical             :: lwrite,lstandardunits,lexist
     integer             :: iargc
     integer,external    :: llen,leap
     lwrite = .FALSE.
@@ -22,9 +22,16 @@ program dat2nc
         stop
     endif
     call getarg(1,file)
+    call getarg(4,ncfile)
     allocate(data(npermax,yrbeg:yrend,0:nensmax))
     if ( lwrite ) print *,'reading from ',trim(file)
     lstandardunits = .false.
+    ! make sure we do not accidentally read from an old netcdf file :-(
+    inquire(file=trim(ncfile),exist=lexist)
+    if ( lexist ) then
+        open(1,file=trim(ncfile))
+        close(1,status='delete')
+    end if
     call readensseries(file,data,npermax,yrbeg,yrend,nensmax,nperyear,mens1,mens, &
     &   var,units,lstandardunits,lwrite)
     open(1,file=file)
@@ -65,12 +72,11 @@ program dat2nc
     do i=1,len(description)
         if ( description(i:i).eq.'_') description(i:i) = ' '
     enddo
-    call getarg(4,file)
 !
 !   and write out
 !
     if ( mens1 == mens ) then
-        call writencseries(file,data,npermax,yrbeg,yrend,nperyear &
+        call writencseries(ncfile,data,npermax,yrbeg,yrend,nperyear &
         &   ,description,comment,var,lvar,units)
     else
         write(0,*) 'dat2nc: error: writing netcdf ensembles not yet ready'
