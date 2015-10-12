cc90fb41e5b6d9373600103ee0df2ef23d29354f
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-10-12 17:32:43 +0200

    ... and now without debugging turned on...

--- transform.f90
+++ transform.f90
@@ -13,7 +13,7 @@ program transform
     character string*100
     logical lwrite
     integer iargc
-    lwrite = .false.
+    lwrite = .true.
 
     if ( iargc().lt.5 ) then
         write(0,*) 'usage: transform infile variable horizon scenario[.subscenario] region'
