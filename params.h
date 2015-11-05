f8854f34a14c24ab62f4e6623d3ef2ba664364ce
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-11-05 13:42:03 +0100

    Added the year zero...

--- params.h
+++ params.h
@@ -2,6 +2,6 @@
         integer nxmax,nymax,nzmax,yrbeg,yrend,nvmax,npermax,ndata         &
      &        ,nensmax,nmodelmax
         parameter (nxmax=1650,nymax=697,nzmax=95,                         &
-     &       yrbeg=1,yrend=2300,nvmax=1,npermax=73,                       &
+     &       yrbeg=0,yrend=2300,nvmax=1,npermax=73,                       &
      &       ndata=npermax*(yrend-yrbeg+1),nensmax=230,                   &
      &       nmodelmax=30)
