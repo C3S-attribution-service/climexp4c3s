9ae2ccdcbd698ddf88b82153ce2b345ba64e0b88
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-11-23 18:04:08 +0100

    Extended nxmax and nymax to be able to deal with 1/4 degree fields.

--- params.h
+++ params.h
@@ -1,7 +1,7 @@
 !       default parameters for the Climate Explorer
         integer nxmax,nymax,nzmax,yrbeg,yrend,nvmax,npermax,ndata         &
      &        ,nensmax,nmodelmax
-        parameter (nxmax=1650,nymax=697,nzmax=95,                         &
+        parameter (nxmax=1650,nymax=720,nzmax=95,                         &
      &       yrbeg=0,yrend=2300,nvmax=1,npermax=73,                       &
      &       ndata=npermax*(yrend-yrbeg+1),nensmax=230,                   &
      &       nmodelmax=30)
