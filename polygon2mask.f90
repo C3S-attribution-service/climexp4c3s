e693f9a9d5d73c8ec5b7b5cfc4fe30d622786291
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2015-12-01 09:57:39 +0100

    Another keepalive...

--- polygon2mask.f90
+++ polygon2mask.f90
@@ -79,6 +79,7 @@ program polygon2mask
   call getarg(iargc(),maskfile)
   !
   ! fill mask array
+  !
   call keepalive1('Generating mask file ',0,0)
   call fillmask(polygon,npol,pole,xx,nx,yy,ny,mask,nxmax,nymax,lwrite)
   !
@@ -209,6 +210,7 @@ subroutine read_polygon(datfile,npol,npolmax,polygon,lwrite)
         polygon(1,npol) = polygon(1,npol1)
         polygon(2,npol) = polygon(2,npol1)
     end if
+    call keepalive1('Reading shapefile',npol,0)
     if ( lwrite ) then
         print *,'read_polygon: found ',npol-npol1,'-edged polygon'
         do ipol=npol1,npol
