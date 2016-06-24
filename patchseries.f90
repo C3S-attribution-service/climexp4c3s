5a5f9dc8193f4089fe1be72014273f61f98f1b96
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-06-24 17:07:50 +0200

    Added option "none" to do nothing, edited the wrong version so "noscale" became 'bias'

--- patchseries.f90
+++ patchseries.f90
@@ -9,11 +9,10 @@ program patchseries
     integer i,j,dy,mo,yr,nperyear,nperyear2,n,nperday,dpm(12)
     real maindata(npermax,yrbeg:yrend),auxdata(npermax,yrbeg:yrend), &
  &       xx(30*(yrend-yrbeg+1)),yy(30*(yrend-yrbeg+1))
-    real scale(12),offset(12),a,b,siga,sigb,chi2,q,sig(1), &
- &       sx(12),sy(12)
-    character var*40,units*20,var2*40,units2*20, &
- &       mainfile*255,auxfile*255,string*100
-    logical lwrite,lreversefit,lnoscale
+    real scale(12),offset(12),a,b,siga,sigb,chi2,q,sig(1),sx(12),sy(12)
+    character var*40,units*20,var2*40,units2*20,mainfile*255,auxfile*255
+    character method*4
+    logical lwrite,lreversefit
     integer iargc
     data dpm /31,29,31,20,31,30,31,31,30,31,30,31/
 
@@ -21,31 +20,32 @@ program patchseries
     lreversefit = .false.
     sx = 0
     sy = 0
-    if ( iargc().lt.2 ) then
-        write(0,*) 'usage: patchseries mainseries auxseries [noscale]'
+    if ( iargc() < 3 ) then
+        write(0,*) 'usage: patchseries mainseries auxseries [regr|bias|none]'
         write(0,*) 'patches holes in mainseries using data from '// &
- &           'auxseries linearly regressed on mainseries (or not)'
+ &           'auxseries linearly regressed on mainseries | biascorrected to mainseries '// &
+ &           '| without corrections (default)'
         stop
     endif
     call getarg(1,mainfile)
     call readseries(mainfile,maindata,npermax,yrbeg,yrend,nperyear &
- &       ,var,units,.false.,lwrite)
+ &       ,var,units,.true.,lwrite)
     call getarg(2,auxfile)
-    call readseries(auxfile,auxdata,npermax,yrbeg,yrend,nperyear2 &
- &       ,var2,units2,.false.,lwrite)
+    call readseries(auxfile,auxdata,npermax,yrbeg,yrend,nperyear2  &
+ &       ,var2,units2,.true.,lwrite)
     if ( nperyear.ne.nperyear2 ) then
         write(0,*) 'patchseries: error: can only handle series '// &
  &           'with the same time resolution, not ',nperyear,nperyear2
         write(*,*) 'patchseries: error: can only handle series '// &
  &           'with the same time resolution, not ',nperyear,nperyear2
-        call exit(-1)
+        call abort
     end if
-    lnoscale = .false.
-    if ( iargc().gt.2 ) then
-        call getarg(3,string)
-        if ( string(1:2) == 'no' ) then
-            lnoscale = .true.
-        end if
+    if ( iargc() == 3 ) then
+        call getarg(3,method)
+        ! "noscale" was recognised by a version that I mistakenly skipped
+        if ( method == 'nosc' ) method = bias
+    else
+        method = 'none'
     end if
 !
 !   determine regression coefficents per month or less
@@ -63,25 +63,35 @@ program patchseries
                     sy(mo) = sy(mo) + yy(n)
                 end if
             end do
-            if ( n.gt.3 ) then
+            if ( n.gt.0 ) then
                 sx(mo) = sx(mo)/n
                 sy(mo) = sy(mo)/n
-            else
-                sx(mo) = 0
-                sy(mo) = 3e33
             end if
-            if ( lnoscale ) then
+            if ( method == 'none' ) then
                 scale(mo) = 1
-                offset(mo) = sy(mo) - sx(mo)
-            else if ( n.lt.7 ) then ! arbitrary
-                scale(mo) = 3e33
-                offset(mo) = 3e33
-            else if ( lreversefit ) then
-                call fit(xx,yy,n,sig,0,offset(mo),scale(mo),siga,sigb,chi2,q)
+                offset(mo) = 0
+            else if ( method == 'bias' ) then
+                if ( n.lt.7 ) then
+                    scale(mo) = 3e33
+                    offset(mo) = 3e33
+                else
+                    scale(mo) = 1
+                    offset(mo) = sy(mo) - sx(mo)
+                end if
+            else if ( method == 'regr' ) then
+                if ( n.lt.7 ) then ! arbitrary
+                    scale(mo) = 3e33
+                    offset(mo) = 3e33
+                else if ( lreversefit ) then
+                    call fit(xx,yy,n,sig,0,offset(mo),scale(mo),siga,sigb,chi2,q)
+                else
+                    call fit(yy,xx,n,sig,0,a,b,siga,sigb,chi2,q)
+                    scale(mo) = 1/b
+                    offset(mo) = -a/b
+                end if
             else
-                call fit(yy,xx,n,sig,0,a,b,siga,sigb,chi2,q)
-                scale(mo) = 1/b
-                offset(mo) = -a/b
+                write(0,*) 'patchseries: error: unknown method ',method
+                call exit(-1)
             end if
         end do
         do yr=yrbeg,yrend
@@ -111,30 +121,41 @@ program patchseries
  &                       auxdata(j,yr).lt.1e33 ) then
                         n = n + 1
                         xx(n) = auxdata(j,yr)
+                        sx(mo) = sx(mo) + xx(n)
                         yy(n) = maindata(j,yr)
                         sy(mo) = sy(mo) + yy(n)
                     end if
                 end do
             end do
-            if ( n.gt.3 ) then
+            if ( n.gt.0 ) then
                 sx(mo) = sx(mo)/n
                 sy(mo) = sy(mo)/n
-            else
-                sx(mo) = 0
-                sy(mo) = 3e33
             end if
-            if ( lnoscale ) then
-                scale(mo) = 0
-                offset(mo) = sy(mo) - sx(mo)
-            else if ( n.lt.7 ) then ! arbitrary
-                scale(mo) = 3e33
-                offset(mo) = 3e33
-            else if ( lreversefit ) then
-                call fit(xx,yy,n,sig,0,offset(mo),scale(mo),siga,sigb,chi2,q)
+            if ( method == 'none' ) then
+                scale(mo) = 1
+                offset(mo) = 0
+            else if ( method == 'bias' ) then
+                if ( n.lt.7 ) then
+                    scale(mo) = 3e33
+                    offset(mo) = 3e33
+                else
+                    scale(mo) = 1
+                    offset(mo) = sy(mo) - sx(mo)
+                end if
+            else if ( method == 'regr' ) then
+                if ( n.lt.7 ) then ! arbitrary
+                    scale(mo) = 3e33
+                    offset(mo) = 3e33
+                else if ( lreversefit ) then
+                    call fit(xx,yy,n,sig,0,offset(mo),scale(mo),siga,sigb,chi2,q)
+                else
+                    call fit(yy,xx,n,sig,0,a,b,siga,sigb,chi2,q)
+                    scale(mo) = 1/b
+                    offset(mo) = -a/b
+                end if
             else
-                call fit(yy,xx,n,sig,0,a,b,siga,sigb,chi2,q)
-                scale(mo) = 1/b
-                offset(mo) = -a/b
+                write(0,*) 'patchseries: error: unknown method ',method
+                call exit(-1)
             end if
         end do
         do yr=yrbeg,yrend
@@ -155,11 +176,18 @@ program patchseries
 !
 !   output
 !
-    call copyheader(mainfile,6)
+    call copyheader_newunits(mainfile,6,units)
     write(6,'(a,g16.6)') '# patched with data from ',trim(auxfile)
-    write(6,'(a,g16.6)') '# using scale,offset,offset at mean '
-    do mo=1,min(12,nperyear)
-        write(6,'(a,4f8.3)') '# ',scale(mo),offset(mo),sx(mo)-sy(mo)
-    end do
+    if ( method == 'bias' ) then
+        write(6,'(a,g16.6)') '# using bias correction '
+        do mo=1,min(12,nperyear)
+            write(6,'(a,4f8.3)') '# ',offset(mo)
+        end do
+    else if ( method == 'regr' ) then
+        write(6,'(a,g16.6)') '# using scale,offset '
+        do mo=1,min(12,nperyear)
+            write(6,'(a,4f8.3)') '# ',scale(mo),offset(mo)
+        end do
+    end if
     call printdatfile(6,maindata,npermax,nperyear,yrbeg,yrend)
 end program
