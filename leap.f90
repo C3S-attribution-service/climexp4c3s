5ca69fccfc9f11e73ff84665f01b0bfe55f7238b
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-11-14 10:49:19 +0100

    Relentless march to f90 format.

---
+++ leap.f90
@@ -0,0 +1,20 @@
+integer function leap(yr)
+
+!	returns 1 if the year yr is not a leap year
+!	returns 2 if it is a leap year
+!	Gregorian calendar assumed throughout
+
+    implicit none
+    integer :: yr
+    if ( mod(yr,4) /= 0 ) then
+        leap = 1
+    elseif ( mod(yr,100) /= 0 ) then
+        leap = 2
+    elseif ( mod(yr,400) /= 0 ) then
+        leap = 1
+    else
+        leap = 2
+    endif
+end function leap
+
+
