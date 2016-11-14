f2f9007d5ba475775c1bed7b92da05494c91e9e8
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-11-14 10:46:38 +0100

    Set values to undef based on missing data in another series

---
+++ setundef.f90
@@ -0,0 +1,37 @@
+program setundef
+
+!   set the values of series 1 that are undefined in series 2 to undefined.
+!   ((like cdo)
+
+    implicit none
+    include 'param.inc'
+    integer :: yr,mo,nperyear,n
+    real :: series1(npermax,yrbeg:yrend),series2(npermax,yrbeg:yrend)
+    logical :: lstandardunits,lwrite
+    character :: file*1024,var*40,lvar*80,units*40
+    integer :: iargc
+
+    if ( iargc() /= 2 ) then
+        write(0,*) 'usage: setundef series1 series2'
+        call exit(-1)
+    end if
+    lwrite = .false.
+    lstandardunits = .false.
+    call getarg(2,file)
+    call readseries(file,series2,npermax,yrbeg,yrend,n,var,units,lstandardunits,lwrite)
+    call getarg(1,file)
+    call readseries(file,series1,npermax,yrbeg,yrend,nperyear,var,units,lstandardunits,lwrite)
+    if ( n /= nperyear ) then
+        write(0,*) 'setundef: error: unequal time scales ',n,nperyear
+        call exit(-1)
+    end if
+    do yr=yrbeg,yrend
+        do mo=1,nperyear
+            if ( series2(mo,yr).gt.1e33 ) then
+                series1(mo,yr) = 3e33
+            end if
+        end do
+    end do
+    call copyheader(file,6)
+    call printdatfile(6,series1,npermax,nperyear,yrbeg,yrend)
+end program
