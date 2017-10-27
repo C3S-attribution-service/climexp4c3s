131aba06e274275d19d480bb45412e0ab631df2f
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2017-10-27 14:58:04 +0200

    Bigger time axis!

--- params.h
+++ params.h
@@ -2,6 +2,6 @@
         integer nxmax,nymax,nzmax,yrbeg,yrend,nvmax,npermax,ndata         &
      &        ,nensmax,nmodelmax
         parameter (nxmax=3600,nymax=1800,nzmax=95,                         &
-     &       yrbeg=0,yrend=2300,nvmax=1,npermax=73,                       &
+     &       yrbeg=0,yrend=2500,nvmax=1,npermax=73,                       &
      &       ndata=npermax*(yrend-yrbeg+1),nensmax=230,                   &
      &       nmodelmax=30)
