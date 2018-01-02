6346762ce8081438893cee8309cb7e85868ac08d
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-01-02 15:51:23 +0100

    - Fixed bug in size itimeaxis - better metadata (i hope) - added keepalive in main loop

--- patchfield.f90
+++ patchfield.f90
@@ -14,7 +14,7 @@ program patchfield
  &      nvars,ivars(6,nvarmax)
     integer mens01,mens11,ncid1,nx1,ny1,nz1,nt1,nperyear1,firstyr1,firstmo1,endian1, &
  &      nvars1,ivars1(6,nvarmax)
-    integer ix,iy,iz,i,j,dy,mo,yr,n,nperday,dpm(12),fyr,lyr,status,itimeaxis(ntmax),ntvarid
+    integer ix,iy,iz,i,j,dy,mo,yr,n,nperday,dpm(12),fyr,lyr,status,itimeaxis(ndata),ntvarid
     real xi(30*(yrend-yrbeg+1)),yi(30*(yrend-yrbeg+1))
     real scale(12),offset(12),a,b,siga,sigb,chi2,q,sig(1),sx(12),sy(12)
     real xx(nxmax),yy(nymax),zz(nzmax),undef
@@ -23,9 +23,9 @@ program patchfield
     logical lreversefit
     character &
         mainfile*255,auxfile*255,outfile*255,datfile*255,datfile1*255,lz(3)*20,ltime*120, &
-        title*2000,history*20000,vars(nvarmax)*40,lvars(nvarmax)*120,svars(nvarmax)*80, &
+        title*2000,title1*2000,history*20000,vars(nvarmax)*40,lvars(nvarmax)*120,svars(nvarmax)*80, &
         units(nvarmax)*40,units1(nvarmax)*40,cell_methods(nvarmax)*40,metadata(2,100)*2000, &
-        metadata2(2,100)*2000
+        metadata1(2,100)*2000
     character method*4
     integer iargc
     data dpm /31,29,31,20,31,30,31,31,30,31,30,31/
@@ -35,7 +35,7 @@ program patchfield
     lwrite = .false.
     lreversefit = .false.
     if ( iargc().lt.3 ) then
-        write(0,*) 'usage: patchfield mainfield auxfield outfield [regr|bias|none]'
+        write(0,*) 'usage: patchfield mainfield auxfield [regr|bias|none] outfield'
         write(0,*) 'patches holes in mainfield using data from '// &
  &           'auxfield using a linear regression, bias correction or straight copy '// &
  &           'for each grid point'
@@ -47,13 +47,13 @@ program patchfield
     call getarg(2,auxfile)
     call getmetadata(auxfile,mens11,mens01,ncid1,datfile1,nxmax,nx1 &
  &       ,xx1,nymax,ny1,yy1,nzmax,nz1,zz1,lz,nt1,nperyear1,firstyr1,firstmo1 &
- &       ,ltime,undef1,endian1,title,history,nvarmax,nvars,vars,ivars1 &
- &       ,lvars,svars,units1,cell_methods,metadata,lwrite)
+ &       ,ltime,undef1,endian1,title1,history,nvarmax,nvars,vars,ivars1 &
+ &       ,lvars,svars,units1,cell_methods,metadata1,lwrite)
     call getarg(1,mainfile)
     call getmetadata(mainfile,mens1,mens,ncid,datfile,nxmax,nx &
  &       ,xx,nymax,ny,yy,nzmax,nz,zz,lz,nt,nperyear,firstyr,firstmo &
  &       ,ltime,undef,endian,title,history,nvarmax,nvars,vars,ivars &
- &       ,lvars,svars,units,cell_methods,metadata2,lwrite)
+ &       ,lvars,svars,units,cell_methods,metadata,lwrite)
     if ( iargc() >= 4 ) then
         call getarg(3,method)
         ! "noscale" was recognised by a version that I mistakenly skipped
@@ -86,12 +86,12 @@ program patchfield
 !
 !   read data
 !
-    if ( lwrite ) print *,'reading ',trim(mainfile)
+    write(0,*) 'Reading ',trim(mainfile)
     call readfield(ncid,mainfile,datfile,mainfield,nx,ny &
  &       ,nz,nperyear,fyr,lyr,0,0,nx,ny,nz,nperyear,fyr,lyr &
  &       ,firstyr,firstmo,nt,undef,endian,vars,units,.true. &
  &       ,lwrite)
-    if ( lwrite ) print *,'reading ',trim(auxfile)
+    write(0,*) 'Reading ',trim(auxfile)
     call readfield(ncid1,auxfile,datfile1,auxfield,nx,ny &
  &       ,nz,nperyear,fyr,lyr,0,0,nx,ny,nz,nperyear,fyr,lyr &
  &       ,firstyr1,firstmo1,nt1,undef1,endian1,vars,units1,.true. &
@@ -102,6 +102,7 @@ program patchfield
     do iz=1,nz
         do iy=1,ny
             do ix=1,nx
+                call keepalive1('point ',ix+(iy-1)*nx+(iz-1)*nx*ny,nx*ny*nz)
                 do yr=fyr,lyr
                     do mo=1,nperyear
                         maindata(mo,yr) = mainfield(ix,iy,iz,mo,yr)
@@ -272,7 +273,15 @@ program patchfield
     else
 !       netcdf output
         undef = 3e33
-        title = trim(title)//' extended with '//trim(auxfile)
+        title = trim(title)//' extended with '//trim(title1)
+        do i=1,1000
+            if ( metadata(1,i) == ' ' ) exit
+        end do
+        do j=1,1000-i
+            if ( metadata1(1,j) == ' ' ) exit
+            metadata(1,j+i-1) = 'aux_'//trim(metadata1(1,j))
+            metadata(1,j+i-1) = metadata1(2,j)
+        end do
         call enswritenc(outfile,ncid,ntvarid,itimeaxis,ndata,nx,xx,ny &
  &           ,yy,nz,zz,lz,nperyear*(lyr-fyr+1),nperyear &
  &           ,fyr,1,ltime,undef,title,history,nvars,vars,ivars &
