7a76d88dd1a0323c4a7efc6f5f74023559958e1d
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-04-17 10:48:26 +0200

    Replaced the Numerical Recipes betai with a wrapper calling the GSL equivalent.

---
+++ betai.f90
@@ -0,0 +1,18 @@
+real function betai(a,b,x)
+!
+!   uses GSL to compute the incomplete beta function
+!
+    use fgsl
+    implicit none
+    real :: a,b,x
+    double precision aa,bb,xx
+
+    if ( x < 1 .or. x > 1 ) then
+        write(0,*) 'betai: error: x not in [0,1]: ',x
+        betai = 3e33
+    end if
+    aa = a
+    bb = b
+    xx = x
+    betai = fgsl_sf_beta_inc(aa,bb,xx)
+end function
