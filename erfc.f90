2576617b66273f9bfcb5f6e221815ccbc701e147
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-04-17 11:39:44 +0200

    Wrapper to (F)GSL

---
+++ erfc.f90
@@ -0,0 +1,18 @@
+real function erfcc(x)
+!
+!   wrapper to compute the complementary error function using (F)GSL
+!
+    use fgsl
+    implicit none
+    real :: x
+    erfcc = fgsl_sf_erfc(dble(x))
+end function erfcc
+real function erfc(x)
+!
+!   wrapper to compute the complementary error function using (F)GSL
+!
+    use fgsl
+    implicit none
+    real :: x
+    erfcc = fgsl_sf_erfc(dble(x))
+end function erfc
