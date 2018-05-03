45a65307a8188a5f92620d139716196365fc522c
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-05-03 19:19:28 +0200

    Moved useful subroutine nperyear2... tp separate file, added units Coverted a few files to f90

--- fieldday2period.f90
+++ fieldday2period.f90
@@ -519,29 +519,6 @@ subroutine adjustnames(oper,nperyear,nperyearnew,lgt,pcut,punits,lvars,cell_meth
     call deletedoublespaces(cell_methods)
 end subroutine 
 
-subroutine nperyear2string(nperyear,string)
-    implicit none
-    integer :: nperyear
-    character string*(*)
-    if ( nperyear > 366 ) then
-        write(string,'(i1,a)') nint(24*365.25/nperyear),'-hr'
-    elseif ( nperyear >= 360 ) then
-        string = 'daily'
-    elseif ( nperyear > 12 ) theN
-        write(string,'(i3,a)') nint(365.25/nperyear),'-dy'
-    elseif ( nperyear == 12 ) then
-        string = 'monthly'
-    elseif ( nperyear == 4 ) then
-        string = 'seasonal'
-    elseif ( nperyear == 1 ) then
-        string = 'annual'
-    else
-        write(0,*) 'nperyear2string: error: cannot handle ',nperyear,' yet'
-        write(*,*) 'nperyear2string: error: cannot handle ',nperyear,' yet'
-        call exit(-1)
-    endif
-end subroutine nperyear2string
-
 subroutine deletedoublespaces(string)
     implicit none
     character*(*) string
---
+++ nperyear2string.f90
@@ -0,0 +1,50 @@
+subroutine nperyear2string(nperyear,string)
+    implicit none
+    integer :: nperyear
+    character string*(*)
+    if ( nperyear > 366 ) then
+        write(string,'(i1,a)') nint(24*365.25/nperyear),'-hr'
+    elseif ( nperyear >= 360 ) then
+        string = 'daily'
+    elseif ( nperyear > 12 ) theN
+        write(string,'(i3,a)') nint(365.25/nperyear),'-dy'
+    elseif ( nperyear == 12 ) then
+        string = 'monthly'
+    elseif ( nperyear == 4 ) then
+        string = 'seasonal'
+    elseif ( nperyear == 4 ) then
+        string = 'biannual'
+    elseif ( nperyear == 1 ) then
+        string = 'annual'
+    else
+        write(0,*) 'nperyear2string: error: cannot handle ',nperyear,' yet'
+        write(*,*) 'nperyear2string: error: cannot handle ',nperyear,' yet'
+        call exit(-1)
+    endif
+end subroutine nperyear2string
+
+subroutine nperyear2units(nperyear,units)
+    implicit none
+    integer :: nperyear
+    character units*(*)
+    if ( nperyear > 366 ) then
+        write(units,'(i1,a)') nint(24*365.25/nperyear),'-hr'
+    elseif ( nperyear >= 360 ) then
+        units = 'dy'
+    elseif ( nperyear > 12 ) theN
+        write(units,'(i3,a)') nint(365.25/nperyear),'-dy'
+    elseif ( nperyear == 12 ) then
+        units = 'mo'
+    elseif ( nperyear == 4 ) then
+        units = '3mo'
+    elseif ( nperyear == 2 ) then
+        units = '6mo'
+    elseif ( nperyear == 1 ) then
+        units = 'yr'
+    else
+        write(0,*) 'nperyear2units: error: cannot handle ',nperyear,' yet'
+        write(*,*) 'nperyear2units: error: cannot handle ',nperyear,' yet'
+        call exit(-1)
+    endif
+end subroutine nperyear2units
+
