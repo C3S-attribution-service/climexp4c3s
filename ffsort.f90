6bbda8d0f909fd58d12746586cb3ec80f0810f4f
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2018-12-17 15:03:07 +0100

    Worksround for bug in fgsl_sort_index, which returns indices that are off by one.

--- ffsort.f90
+++ ffsort.f90
@@ -6,6 +6,7 @@ subroutine ffsort(a,ii,n)
     implicit none
     integer :: n,ii(n)
     real :: a(n)
+    integer :: i
     integer(fgsl_size_t) :: dim
     integer(fgsl_size_t), allocatable :: p(:)
     real(fgsl_double), allocatable :: aa(:)
@@ -14,7 +15,7 @@ subroutine ffsort(a,ii,n)
     allocate(aa(n),p(n))
     aa = a
     call fgsl_sort_index(p,aa,1_fgsl_size_t,dim)
-    ii = p
+    ii = p+1 ! the routine returns C offset-0 indices rather than Fortran offset-1
     deallocate(aa,p)
 end subroutine ffsort
 subroutine ffsort_d(a,ii,n)
@@ -33,6 +34,6 @@ subroutine ffsort_d(a,ii,n)
     allocate(aa(n),p(n))
     aa = a
     call fgsl_sort_index(p,aa,1_fgsl_size_t,dim)
-    ii = p
+    ii = p+1 ! the routine returns C offset-0 indices rather than Fortran offset-1
     deallocate(aa,p)
 end subroutine ffsort_d
