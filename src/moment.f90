46abb1a513edc1efbedd342521e45d0869ddf7d2
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-01-18 17:47:09 +0100

    Fixed bug in moments: do not call GSL routine with too few values.

--- src/moment.f90
+++ src/moment.f90
@@ -16,37 +16,34 @@ subroutine moment(data,n,ave,adev,sdev,var,skew,curt)
         print *,'entering moment, n = ',n
         print *,'data = ',data(1:n)
     endif
-    if ( n <= 1 ) then
-        write(0,*) 'moment: warning: n < 2: ',n
-        ave = 3e33
-        adev = 3e33
-        sdev = 3e33
-        var = 3e33
-        skew = 3e33
-        curt = 3e33
-        return
-    end if
-!
+    ave = 3e33
+    adev = 3e33
+    sdev = 3e33
+    var = 3e33
+    skew = 3e33
+    curt = 3e33
+ !
 !   call FGSL routines
 !    
     allocate(ddata(n))
     ddata = data ! C double
     dim = n ! correct data type I think
+    if ( n < 1 ) return
     dave = fgsl_stats_mean(ddata,1_fgsl_size_t,dim)
-    dvar = fgsl_stats_variance_m(ddata,1_fgsl_size_t,dim,dave)
-    dsdev = sqrt(dvar)
-    dadev = fgsl_stats_absdev_m(ddata,1_fgsl_size_t,dim,dave)
-    dskew = fgsl_stats_skew_m_sd(ddata,1_fgsl_size_t,dim,dave,dsdev)
-    dcurt = fgsl_stats_kurtosis_m_sd(ddata,1_fgsl_size_t,dim,dave,dsdev)
-    deallocate(ddata)
-!
-!   back to single precision
-!
     ave = dave
-    adev = dadev
-    sdev = dsdev
+    if ( n < 2 ) return    
+    dvar = fgsl_stats_variance_m(ddata,1_fgsl_size_t,dim,dave)
     var = dvar
+    dsdev = sqrt(dvar)
+    sdev = dsdev
+    dadev = fgsl_stats_absdev_m(ddata,1_fgsl_size_t,dim,dave)
+    adev = dadev
+    if ( n < 3 ) return
+    dskew = fgsl_stats_skew_m_sd(ddata,1_fgsl_size_t,dim,dave,dsdev)
     skew = dskew
+    if ( n < 4 ) return
+    dcurt = fgsl_stats_kurtosis_m_sd(ddata,1_fgsl_size_t,dim,dave,dsdev)
     curt = dcurt
+    deallocate(ddata)!
 
 end subroutine moment
