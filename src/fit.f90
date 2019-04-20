db0baef86d5eb6cef30532d1cdb7035392096993
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-04-20 17:45:02 +0200

    The GSL routine also crashes on n=2, so do that ourselves.

--- src/fit.f90
+++ src/fit.f90
@@ -30,6 +30,23 @@ subroutine fit(xx,yy,ndata,sig,mwt,a,b,siga,sigb,chi2,q)
         chi2 = 0
         return
     end if
+    if ( ndata == 2 ) then
+        if ( xx(1) == xx(2) ) then
+            if ( yy(1) == yy(2) ) then
+                a = yy(1)
+                b = 0
+            else
+                a = 3e33
+                b = 3e33
+                chi2 = 3e33
+            end if
+            return
+        end if
+        a = (yy(2) - yy(1))/(xx(2) - xx(1))
+        b = yy(1) - a*xx(1)
+        chi2 = 0
+        return
+    end if
     if ( sum(xx**2) == 0 ) then
         return
     end if
