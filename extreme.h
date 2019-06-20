14f7354048e7b505cc692cffa58d70a0daeae3f0
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-06-20 22:13:13 +0200

    Now generated.

--- extreme.h
+++
@@ -1,4 +0,0 @@
-      integer      yrbeg,yrend,calyrbeg,calyrend,maxstat
-      parameter    (yrbeg=1700,yrend=2020,calyrbeg=1961,calyrend=1990)
-      integer      nseason
-      parameter    (nseason=19)
