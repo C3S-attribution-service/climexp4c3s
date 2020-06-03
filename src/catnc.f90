1d7f4ad5bc04260bc5e70fdf7769516f3e559f1a
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2020-06-03 19:12:32 +0200

    Now also works for daily data.

--- src/catnc.f90
+++ src/catnc.f90
@@ -9,8 +9,8 @@
     include 'netcdf.inc'
     integer :: nvarmax,ntmax,fyr,lyr
     parameter(nvarmax=1,ntmax=366*1000,fyr=1850,lyr=2100)
-    integer :: nx,ny,nz,nt,nperyear,yrbegin,mobegin,endian,nvars,ivars(2,nvarmax), &
-        jvars(6,nvarmax)
+    integer :: nx,ny,nz,nt,nnt,nperyear,yrbegin,mobegin,endian,nvars,ivars(2,nvarmax), &
+        jvars(6,nvarmax),nperday,dy,dy1
     integer :: status,ncid,i,j,k,itimeaxis(ntmax),dpm(12,2), &
         ix,iy,iz,it,iv,localendian,ntvarid,nargs,fyrall,lyrall,f1yr,l1yr,iarg, &
         yr,mo,nens1,nens2
@@ -20,6 +20,7 @@
     character :: ncfile*255,title*1023,lz(3)*20,ltime*120,history*20000, &
         cell_methods(100)*100,metadata(2,100)*2000
     logical :: lwrite,tdefined(ntmax)
+    integer,external :: leap
     data dpm &
     /31,28,31,30,31,30,31,31,30,31,30,31 &
     ,31,29,31,30,31,30,31,31,30,31,30,31/
@@ -84,17 +85,33 @@
     call get_command_argument(nargs,ncfile)
     nt = nperyear*(lyrall - fyrall + 1)
     if ( lwrite ) print *,'writing metadata'
+    nnt = nt
+    nperday = 1
+    if ( nperyear >= 366 ) then
+        nperday = nint(nperyear/366.)
+        if ( nperyear/nperday == 366 ) then
+            nnt = 0
+            do yr=fyrall,lyrall
+                if ( leap(yr) == 1 ) then
+                    nnt = nnt + 365
+                else
+                    nnt = nnt + 366
+                end if
+            end do
+        end if
+    end if
     call enswritenc(ncfile,ncid,ntvarid,itimeaxis,ntmax,nx,xx,ny,yy &
-        ,nz,zz,lz,nt,nperyear,fyrall,1,ltime,undef,title,history,nvars &
+        ,nz,zz,lz,nnt,nperyear,fyrall,1,ltime,undef,title,history,nvars &
         ,vars,ivars,lvars,svars,units,cell_methods,metadata,0,0) ! no ensembles yet
     it = 0
     if ( lwrite ) print *,'writing data'
     do yr=fyrall,lyrall
-        do mo=1,nperyear
+        do dy=1,nperyear
+            if ( leap(yr) == 1 .and. dy/nperday == 60 ) cycle
             it = it + 1
             call keepalive1('Writing slice ',it,(lyrall-fyrall+1)*nperyear)
             call writencslice(ncid,ntvarid,itimeaxis,ntmax, &
-                ivars(1,1),field(1,1,1,mo,yr),nx,ny,nz,nx,ny,nz,it,1)
+                    ivars(1,1),field(1,1,1,dy,yr),nx,ny,nz,nx,ny,nz,it,1)
         end do
     enddo
     status = nf_close(ncid)
