76efa344140f9cd5e80ac2dc592047da419c146f
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2020-07-03 12:24:59 +0200

    Fixed copy/paste error.

--- src/synthesis.f90
+++ src/synthesis.f90
@@ -160,7 +160,7 @@ program synthesis
         do j=1,len(line)
             if ( ( ichar(line(j:j)).ge.ichar('a') .and. ichar(line(j:j)).le.ichar('z') &
               .or. ichar(line(j:j)).ge.ichar('A') .and. ichar(line(j:j)).le.ichar('Z') ) & 
-                .and. line(j:j+1) /= 'E-' .and. line(1:2) /= 'E+' &
+                .and. line(j:j+1) /= 'E-' .and. line(j:j+1) /= 'E+' &
               .or. line(j:j+2) == '20C' ) then
                 names(n) = line(j:)
                 exit
